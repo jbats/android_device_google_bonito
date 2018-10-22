0362ddb0cb7fa42a865058eb8b747659cc7ef102
Nathan Chancellor <nathan@dirtyunicorns.com>
2018-10-22 12:56:33 -0400

    b4s4: Allow kernel to compile inline.
    * Do not use prebuilt kernel
    
    Change-Id: I3655b238170f923a1c1d10fd662eb697ba93767f

--- device.mk
+++ device.mk
@@ -55,19 +55,12 @@ $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-    LOCAL_KERNEL := device/google/bonito-kernel/Image.lz4
-else
-    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_SHIPPING_API_LEVEL := 28
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
     $(LOCAL_PATH)/init/init.recovery.hardware.rc:recovery/root/init.recovery.$(PRODUCT_PLATFORM).rc
 
 #per device
